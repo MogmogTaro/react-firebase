4f70263da0514550a29955f1c1d6f7890b5bdb81
UCDM <palama.ucdm.1212@gmail.com>
2022-01-13 13:57:21 +0900

    css追加

--- src/components/Facility.tsx
+++ src/components/Facility.tsx
@@ -1,10 +1,23 @@
 import { Container, Paper, TextField } from "@material-ui/core";
 import React from "react";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const useStyle = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+    },
+  },
+  paper: {
+    padding: theme.spacing(1),
+  },
+}));
 
 export const Facility: React.FC = () => {
+  const style = useStyle();
   return (
-    <Container maxWidth="sm">
-      <Paper>
+    <Container maxWidth="sm" className={style.root}>
+      <Paper className={style.paper}>
         <TextField label="設備名" fullWidth />
         <TextField label="詳細" fullWidth multiline />
       </Paper>
