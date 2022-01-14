a0636f4f3d5bb693b480bd5cf601433bd830358d
UCDM <palama.ucdm.1212@gmail.com>
2022-01-14 10:33:07 +0900

    routingバージョンダウン

--- src/components/Routing.tsx
+++ src/components/Routing.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import { Reservation } from "./Reservation";
 import { Facility } from "./Facility";
 import { ReservationList } from "./ReservationList";
 
 export const Routing: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/reservation" element={<Reservation />} />
-      <Route path="/facility" element={<Facility />} />
-      <Route path="/" element={<ReservationList />} />
-    </Routes>
+    <Switch>
+      <Route path="/reservation" component={Reservation} />
+      <Route path="/facility" component={Facility} />
+      <Route path="/" component={ReservationList} />
+    </Switch>
   );
 };
